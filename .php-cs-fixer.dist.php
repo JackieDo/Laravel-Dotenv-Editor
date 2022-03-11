2a573936b2cd90981c41031732a4a2b9eaeb5a3d
JackieDo <anhvudo@gmail.com>
2022-03-12 03:06:56 +0700

    Create .php-cs-fixer.dist.php

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,64 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+$rules = [
+    '@PhpCsFixer' => true,
+    'binary_operator_spaces' => [
+        // 'default' => 'align_single_space_minimal',
+    ],
+    'blank_line_before_statement' => [
+        'statements' => [
+            // 'break',
+            'case',
+            'continue',
+            'declare',
+            'default',
+            'phpdoc',
+            'do',
+            'exit',
+            'for',
+            'foreach',
+            'while',
+            'goto',
+            'if',
+            'include',
+            'include_once',
+            'require',
+            'require_once',
+            'return',
+            'switch',
+            'throw',
+            'try',
+        ],
+    ],
+    'concat_space' => [
+        'spacing' => 'one',
+    ],
+    'new_with_braces' => false,
+    'no_empty_comment' => false,
+    'no_superfluous_phpdoc_tags' => false,
+    'multiline_whitespace_before_semicolons' => [
+        'strategy' => 'no_multi_line',
+    ],
+    'phpdoc_add_missing_param_annotation' => [
+        'only_untyped' => false
+    ],
+    'phpdoc_no_empty_return' => false,
+    'phpdoc_no_package' => false,
+];
+
+$config = new Config;
+
+return $config
+    ->setFinder($finder)
+    ->setRules($rules)
+    ->setUsingCache(true);
